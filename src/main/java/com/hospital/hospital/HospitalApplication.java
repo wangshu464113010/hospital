63ea7d6b600a85d03844abfec63438f1b9a45b85
wangshu464113010@outlook.com <kwniix3.1415926>
2019-01-16 10:02:51 +0800

    wangshu commit 项目环境搭建 2019-01-16 10:02

--- src/main/java/com/hospital/hospital/HospitalApplication.java
+++ src/main/java/com/hospital/hospital/HospitalApplication.java
@@ -1,18 +1,15 @@
 package com.hospital.hospital;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.hospital.hospital.mapper")
 public class HospitalApplication {
 
     public static void main(String[] args) {
-
-
         SpringApplication.run(HospitalApplication.class, args);
-
-
-
     }
 
 }
---
+++ src/main/java/com/hospital/hospital/controller/HelloController.java
@@ -0,0 +1,33 @@
+package com.hospital.hospital.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @program: hospital
+ * @description: 跳转页面
+ * @author: wangshu
+ * @create: 2019-01-16
+ **/
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("{page}")//跳转页面
+    public String toPage(@PathVariable String page){
+        return page;
+    }
+
+//    @RequestMapping("/index")
+//    public String toIndex(){
+//        System.out.println("123456");
+//        return "index";
+//    }
+//    @RequestMapping("/main")
+//    public String toPage(){
+//        System.out.println("123456");
+//        return "main";
+//    }
+
+}
